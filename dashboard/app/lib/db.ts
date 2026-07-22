404953463b02445298989d36739f8a3c787677ce
Shamiul40 <shamiul4040@gmail.com>
2026-07-22 23:09:45 +0600

    database connection check function create

--- dashboard/app/lib/db.ts
+++ dashboard/app/lib/db.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
+
+export async function checkDatabaseConnection() : Promise<boolean> {
+    try{
+        await prisma.$queryRaw`select 1`;
+        return true
+    } catch (error) {
+        console.error(`database connection failed : ${error}`)
+    }
+}
