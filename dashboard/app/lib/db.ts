02c5e35f93c56e432d3946fa9ed97c3f74795ad9
Shamiul40 <shamiul4040@gmail.com>
2026-07-23 22:18:24 +0600

    practice code delete

--- dashboard/app/lib/db.ts
+++ dashboard/app/lib/db.ts
@@ -1,34 +1,20 @@
-// import { PrismaClient } from "@prisma/client";
-
-import { Prisma, PrismaClient } from "@prisma/client"
-
-// export const prisma = new PrismaClient();
-
-// export async function checkDatabaseConnection() : Promise<boolean> {
-//     try{
-//         await prisma.$queryRaw`select 1`;
-//         return true
-//     } catch (error) {
-//         console.error(`database connection failed : ${error}`)
-//         return false
-//     }
-// }
 
 
+import { PrismaClient } from "@prisma/client"
 
-// for practice
+export const prisma = new PrismaClient();
 
-
-export async function databaseConnection() : Promise<boolean> {
-    
-    const prisma = new PrismaClient();
-
+export async function checkDatabaseConnection() : Promise<boolean> {
     try{
-        await prisma.$queryRaw`select 1`
-        return true;
-    } catch(error) {
-        console.error(`database connection failed, ${error}`)
+        await prisma.$queryRaw`select 1`;
+        return true
+    } catch (error) {
+        console.error(`database connection failed : ${error}`)
         return false
     }
-
 }
+
+
+
+
+
